c4349e276b949945d8e8d8a3223db3cce07f2f06
alexrussell159 <alexrussell159@gmail.com>
2026-03-26 10:02:35 -0500

    other

--- public_html/js/loadComponents.js
+++ public_html/js/loadComponents.js
@@ -29,3 +29,9 @@
         document.body.style.overflow = '';
       });
     });
+
+async function loadHeader() {
+  const res = await fetch('components/header.html');
+  const html = await res.text();
+  document.getElementById('header').innerHTML = html;
+}
