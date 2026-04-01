4e304c23296764ed4dd22f66f460ca17ed53f166
alexrussell159 <alexrussell159@gmail.com>
2026-04-01 16:25:51 -0500

    rebuild

--- .eleventy.js
+++ .eleventy.js
@@ -1,26 +1,37 @@
 module.exports = function(eleventyConfig) {
+  eleventyConfig.addPassthroughCopy({ "public_html/css": "css" });
+  eleventyConfig.addPassthroughCopy({ "public_html/js": "js" });
+  eleventyConfig.addPassthroughCopy({ "public_html/assets": "assets" });
+  eleventyConfig.addPassthroughCopy({ "public_html/components": "components" });
 
-  eleventyConfig.addPassthroughCopy("public_html/css");
-  eleventyConfig.addPassthroughCopy("public_html/js");
-  eleventyConfig.addPassthroughCopy("public_html/assets");
-  eleventyConfig.addPassthroughCopy("public_html/components");
-
-  eleventyConfig.addPassthroughCopy("public_html/about");
-  eleventyConfig.addPassthroughCopy("public_html/services");
-  eleventyConfig.addPassthroughCopy("public_html/pricing");
-  eleventyConfig.addPassthroughCopy("public_html/case-studies");
-  eleventyConfig.addPassthroughCopy("public_html/free-strategy-call");
+  eleventyConfig.addPassthroughCopy({ "public_html/about": "about" });
+  eleventyConfig.addPassthroughCopy({ "public_html/services": "services" });
+  eleventyConfig.addPassthroughCopy({ "public_html/pricing": "pricing" });
+  eleventyConfig.addPassthroughCopy({ "public_html/case-studies": "case-studies" });
+  eleventyConfig.addPassthroughCopy({ "public_html/free-strategy-call": "free-strategy-call" });
 
   eleventyConfig.addPassthroughCopy({ "public_html/index.html": "index.html" });
+
   eleventyConfig.addPassthroughCopy({ "public_html/admin": "admin" });
 
-  // copy post-local images
-  eleventyConfig.addPassthroughCopy("public_html/src/blog/**/*.png");
-  eleventyConfig.addPassthroughCopy("public_html/src/blog/**/*.jpg");
-  eleventyConfig.addPassthroughCopy("public_html/src/blog/**/*.jpeg");
-  eleventyConfig.addPassthroughCopy("public_html/src/blog/**/*.webp");
-  eleventyConfig.addPassthroughCopy("public_html/src/blog/**/*.gif");
-  eleventyConfig.addPassthroughCopy("public_html/src/blog/**/*.svg");
+  eleventyConfig.addPassthroughCopy({
+    "public_html/src/blog/**/*.png": "blog"
+  });
+  eleventyConfig.addPassthroughCopy({
+    "public_html/src/blog/**/*.jpg": "blog"
+  });
+  eleventyConfig.addPassthroughCopy({
+    "public_html/src/blog/**/*.jpeg": "blog"
+  });
+  eleventyConfig.addPassthroughCopy({
+    "public_html/src/blog/**/*.webp": "blog"
+  });
+  eleventyConfig.addPassthroughCopy({
+    "public_html/src/blog/**/*.gif": "blog"
+  });
+  eleventyConfig.addPassthroughCopy({
+    "public_html/src/blog/**/*.svg": "blog"
+  });
 
   return {
     dir: {
